f0dd006b96fa40cb32a7909a7c03234d3ae96ed9
kerllsname <kirillpotapovnow@gmail.com>
2020-07-25 13:18:42 +0300

    random word and description

--- words/src/components/get_word.js
+++ words/src/components/get_word.js
@@ -1,51 +1,59 @@
 import React from "react";
 import axios from "axios";
 
-function RandWord(props) {
+function GetWords(props) {
+
   return (
     <div>
-      <div>
-        {props.word.value}
+      <div>{
+        props.word.value
+      }
       </div>
-      <div>
-        {props.word.description}
+      <div>{
+        props.word.description
+      }
       </div>
     </div>
   )
 }
 
 export default class ListOfWords extends React.Component {
+
   state = {
     words: []
   }
 
   componentDidMount() {
+    
     axios.get("https://aliases.herokuapp.com/api/words").then(
-          (res) => {
-            const words = res.data.data
-            this.setState({ words })
-          }
-        ) 
-  }
-
-  renderWord() {
-    return (
-      this.state.words.map(
-        (word) => {
-          return (
-            <RandWord word={word} key={word.id} />
-          )
-        }
-      )
+      (res) => {
+        const words = res.data.data
+        this.setState({ words })
+      }
     )
   }
+  
+  renderWord() {
 
+    const { words } = this.state;
+    var index = Math.floor(Math.random() * Math.floor(words.length));
+    const word = words[index];
 
+    return (
+      <GetWords word={word} key={word.id} />
+    )
+  }
+  
   render() {
+
+    const { words } = this.state;
+
     return (
       <div> {
-        this.renderWord()
-        }
+        words.length
+          ? this.renderWord()
+          : undefined
+      }
       </div>
     )
   }
